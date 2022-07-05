16ec877df6df688d4e900f69492edac47eefb293
suchetamahata <nosuchetamahatano@gmail.com>
2022-07-05 13:16:57 +0530

    build file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { taskRouter } from "./routes/taskRoutes"
 import mongoose from 'mongoose'
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
 
 
 const app = express()
@@ -12,14 +13,20 @@ app.use(express.json())
 app.use(cors())
 app.use('/', userRouter)
 app.use('/get-behaviours', nameRouter)
-app.use('/task',authentication, taskRouter )
+app.use('/task', authentication, taskRouter)
+app.use(express.static(path.join(__dirname, './build')))
 
-app.listen(4040, ()=> console.log("app listening on port 4040"))
 
-mongoose.connect("mongodb+srv://sucheta:moonstar*@cluster0.uolwybx.mongodb.net/eubrics_data?retryWrites=true&w=majority"|| '', (err: any) => {
-    if (err) {
-      console.log('Cant connect db')
-    } else {
-      console.log('Connected to db')
-    }
-  })
+app.listen(4040, () => console.log("app listening on port 4040"))
+
+mongoose.connect("mongodb+srv://sucheta:moonstar*@cluster0.uolwybx.mongodb.net/eubrics_data?retryWrites=true&w=majority" || '', (err: any) => {
+  if (err) {
+    console.log('Cant connect db')
+  } else {
+    console.log('Connected to db')
+  }
+})
+
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
